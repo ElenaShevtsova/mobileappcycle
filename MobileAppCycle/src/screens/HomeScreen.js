654748a16d1532dfd8aec78144a8eb74014d759c
shevtsova <e.shevtsova@sumatosoft.com>
2022-08-10 17:12:14 +0300

    change text

--- MobileAppCycle/src/screens/HomeScreen.js
+++ MobileAppCycle/src/screens/HomeScreen.js
@@ -5,7 +5,7 @@ export const HomeScreen = ({ navigation }) => {
     return <View style={styles.sectionContainer}>
         <Text style={styles.title}>Home Screen</Text>
         <Button
-            title="Go to Details"
+            title="Go to Help Screen"
             onPress={() => navigation.navigate('Help')}
         />
     </View>
